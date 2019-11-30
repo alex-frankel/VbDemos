e3f14ca5c5008b45329d0d96faead40b81a19e22
Alexander Frankel <alfran@microsoft.com>
2019-11-30 08:58:16 -0500

    added more complex deployment script

---
+++ misc/create-cert.ps1
@@ -0,0 +1,52 @@
+param(
+    [string] [Parameter(Mandatory=$false)] $vaultName,
+    [string] [Parameter(Mandatory=$false)] $certificateName,
+    [string] [Parameter(Mandatory=$false)] $subjectName
+)
+
+Get-AzContext
+
+$ErrorActionPreference = 'Stop'
+$DeploymentScriptOutputs = @{}
+
+$existingCert = Get-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName
+
+if ($existingCert -and $existingCert.Certificate.Subject -eq $subjectName) {
+
+    Write-Host \"Certificate $certificateName in vault $vaultName is already present, updating issuance policy\"
+
+    $DeploymentScriptOutputs['certThumbprint'] = $existingCert.Thumbprint
+    $existingCert | Out-String
+}
+else {
+
+    $policy = New-AzKeyVaultCertificatePolicy -SubjectName $subjectName -IssuerName Self -ValidityInMonths 12 -Verbose
+
+    # private key is added as a secret that can be retrieved in the ARM template
+    Add-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName -CertificatePolicy $policy -Verbose
+
+    $newCert = Get-AzKeyVaultCertificate -VaultName $vaultName -Name $certificateName
+
+    # it takes a few seconds for KeyVault to finish
+    $tries = 0 
+    do {
+        Write-Host 'Waiting for certificate creation completion...'
+        Start-Sleep -Seconds 10
+        $operation = Get-AzKeyVaultCertificateOperation -VaultName $vaultName -Name $certificateName
+        $tries++
+
+        if ($operation.Status -eq 'failed')
+        {
+          throw \"Creating certificate $certificateName in vault $vaultName failed with error $($operation.ErrorMessage)\"
+        }
+
+        if ($tries -gt 120)
+        {
+          throw \"Timed out waiting for creation of certificate $certificateName in vault $vaultName\"
+        }
+
+    } while ($operation.Status -ne 'completed')
+
+    $DeploymentScriptOutputs['certThumbprint'] = $newCert.Thumbprint
+    $newCert | Out-String
+}
